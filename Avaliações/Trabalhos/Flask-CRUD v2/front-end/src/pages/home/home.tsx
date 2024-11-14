052b54e325391e94c3830eea77a92ad57f06d3d2
Vitor Freitas <vitordeavilafreitas@gmail.com>
2024-11-14 06:03:14 -0300

    fix: ajustes finais

--- Avaliações/Trabalhos/Flask-CRUD v2/front-end/src/pages/home/home.tsx
+++ Avaliações/Trabalhos/Flask-CRUD v2/front-end/src/pages/home/home.tsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react"
 import "./home.css"
-import { TaskCreate, TaskGetAll } from "../../requests/task.request";
+import { TaskCheck, TaskCreate, TaskDelete, TaskEdit, TaskGetAll } from "../../requests/task.request";
 import { useNavigate } from "react-router-dom";
 import Modal from '@mui/material/Modal';
+
+interface ITask {
+    id: number,
+    title: string,
+    description: string,
+    checked: boolean,
+    user_id: number
+}
 
 export const Home = () => {
     const [tasks, setTasks] = useState([]);
     const [tasksChecked, setTasksChecked] = useState([]);
     const [toDoOrNot, setToDoOrNot] = useState(true);
-    const [taskClicked, setTaskClicked] = useState(true);
+    const [taskClicked, setTaskClicked] = useState<ITask>();
     const [modal, setModal] = useState(false);
     const [modalCheck, setModalCheck] = useState(false);
     const [modalEdit, setModalEdit] = useState(false);
@@ -16,7 +24,6 @@ export const Home = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const token = localStorage?.getItem("token")
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -38,22 +45,67 @@ export const Home = () => {
         }
     }
 
+    const checkTask = (event: any) => {
+        event.preventDefault();
+        try {
+            if (token && taskClicked) {
+                const checked_task = TaskCheck(token, taskClicked.id)
+                console.log(checked_task)
+                navigate(0)
+            }
+
+        } catch {
+            console.log('error')
+        }
+    }
+
+    const editTask = (event: any) => {
+        event.preventDefault();
+        try {
+            if (token && (title || description) && taskClicked) {
+                const updated_task = TaskEdit(token, taskClicked.id, title, description)
+                console.log(updated_task)
+                navigate(0)
+            }
+
+        } catch {
+            console.log('error')
+        }
+    }
+
+    const deleteTask = (event: any) => {
+        event.preventDefault();
+        try {
+            if (token && taskClicked) {
+                const deleted_task = TaskDelete(token, taskClicked.id)
+                console.log(deleted_task)
+                navigate(0)
+            }
+
+        } catch {
+            console.log('error')
+        }
+    }
+
 
     const handleCallGets = async () => {
         try {
             if (token) {
                 const requestTasks = await TaskGetAll(token)
                 console.log(requestTasks)
-                setTasks(requestTasks)
+                const listChecked = requestTasks.filter((task: ITask) => task.checked)
+
+                const listNotChecked = requestTasks.filter((task: ITask) => !task.checked)
+
+                setTasks(listNotChecked)
+                setTasksChecked(listChecked)
             }
 
         } catch {
             console.log("error home useEffect")
         }
     }
-    const onClose = () => {
-        setAnchorEl(null);
-    };
+
     useEffect(() => {
         handleCallGets()
     }, [])
@@ -62,7 +114,7 @@ export const Home = () => {
         <body>
             <Modal
                 open={modal}
-                onClose={() => { setModal(false), onClose() }}
+                onClose={() => setModal(false)}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
@@ -97,26 +149,91 @@ export const Home = () => {
                         <div style={{ width: "100%" }}>
                             <button className="button-form" type="submit">Enviar</button>
                         </div>
+                        <div style={{ width: "100%" }}>
+                            <button className="button-form-red" onClick={() => setModal(false)}>Cancelar</button>
+                        </div>
                     </form>
                 </div>
             </Modal>
             <Modal
-                open={modal}
-                onClose={() => { setModal(false), onClose() }}
+                open={modalCheck}
+                onClose={() => setModalCheck(false)}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
                 <div className="modal">
-                    <form className="modal-display" onSubmit={createTask}>
+                    <form className="modal-display" onSubmit={checkTask}>
                         <div className="display-form">
-                            <h1>Deseja concluir a task {taskClicked}?</h1>
+                            <h1>Deseja concluir a task {taskClicked?.title}?</h1>
                         </div>
 
                         <div style={{ width: "100%" }}>
                             <button className="button-form" type="submit">Enviar</button>
                         </div>
                         <div style={{ width: "100%" }}>
-                            <button className="button-form-red" type="submit">Não</button>
+                            <button className="button-form-red" onClick={() => setModalCheck(false)}>Cancelar</button>
+                        </div>
+                    </form>
+                </div>
+            </Modal>
+            <Modal
+                open={modalDelete}
+                onClose={() => setModalDelete(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <div className="modal">
+                    <form className="modal-display" onSubmit={deleteTask}>
+                        <div className="display-form">
+                            <h1>Deseja deletar a task {taskClicked?.title}?</h1>
+                        </div>
+
+                        <div style={{ width: "100%" }}>
+                            <button className="button-form" type="submit">Enviar</button>
+                        </div>
+                        <div style={{ width: "100%" }}>
+                            <button className="button-form-red" onClick={() => setModalDelete(false)}>Cancelar</button>
+                        </div>
+                    </form>
+                </div>
+            </Modal>
+            <Modal
+                open={modalEdit}
+                onClose={() => setModalEdit(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <div className="modal">
+                    <form className="modal-display" onSubmit={editTask}>
+                        <div className="display-form">
+                            <h1>Editando a Task {taskClicked?.title}</h1>
+                            <div className="display-inputs">
+                                <p>Title</p>
+                                <input
+                                    type="text"
+                                    id="title"
+                                    name="title"
+                                    onChange={(event) => { setTitle(event.target.value) }}
+                                    placeholder={taskClicked?.title}
+                                />
+                            </div>
+                            <div className="display-inputs">
+                                <p>Description</p>
+                                <input
+                                    id="description"
+                                    name="description"
+                                    onChange={(event) => { setDescription(event.target.value) }}
+                                    type="text"
+                                    placeholder={taskClicked?.description}
+                                />
+                            </div>
+                        </div>
+
+                        <div style={{ width: "100%" }}>
+                            <button className="button-form" type="submit">Enviar</button>
+                        </div>
+                        <div style={{ width: "100%" }}>
+                            <button className="button-form-red" onClick={() => { setModalEdit(false), setTitle(""), setDescription("") }}>Cancelar</button>
                         </div>
                     </form>
                 </div>
@@ -130,15 +247,15 @@ export const Home = () => {
                 <div className="task-container">
                     <h2>Lista de Tarefas</h2>
                     <ul className="task-list">
-                        {tasks ? tasks.map((value: any, index: number) => {
+                        {tasks[0] ? tasks?.map((value: any, index: number) => {
                             return (
                                 <li className="task-item">
                                     <span className="task-title">{value.title}</span>
                                     <p className="task-description">{value.description}</p>
                                     <div className="task-actions">
-                                        <button className="btn check-btn" onClick={() => { setModalCheck(true), setTaskClicked(value.title) }}>✔️</button>
-                                        <button className="btn edit-btn" onClick={() => { setModalEdit(true), setTaskClicked(value.title) }}>✏️</button>
-                                        <button className="btn delete-btn" onClick={() => { setModalDelete(true), setTaskClicked(value.title) }}>❌</button>
+                                        <button className="btn check-btn" onClick={() => { setModalCheck(true), setTaskClicked(value) }}>✔️</button>
+                                        <button className="btn edit-btn" onClick={() => { setModalEdit(true), setTaskClicked(value) }}>✏️</button>
+                                        <button className="btn delete-btn" onClick={() => { setModalDelete(true), setTaskClicked(value) }}>❌</button>
                                     </div>
                                 </li>
                             )
@@ -150,15 +267,13 @@ export const Home = () => {
                 <div className="task-container">
                     <h2>Tarefas feitas.</h2>
                     <ul className="task-list">
-                        {tasks ? tasks.map((value: any, index: number) => {
+                        {tasksChecked[0] ? tasksChecked.map((value: any, index: number) => {
                             return (
                                 <li className="task-item">
                                     <span className="task-title">{value.title}</span>
                                     <p className="task-description">{value.description}</p>
                                     <div className="task-actions">
-                                        <a href="/task/delete/{{task.id}}">
-                                            <button className="btn delete-btn">❌</button>
-                                        </a>
+                                        <button className="btn delete-btn" onClick={() => { setModalDelete(true), setTaskClicked(value) }}>❌</button>
                                     </div>
                                 </li>
                             )
